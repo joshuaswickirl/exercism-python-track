ca286a53ab6843f0c05c826c3de2c0ce5041b5c6
Joshua Swick <joshuaswickirl@gmail.com>
2019-08-03 10:58:18 -0600

    Refactor to use splitlines and list comp

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,16 +1,10 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        matrix_rows_string = (matrix_string.split('\n'))
-        self.matrix = []
-        for row in matrix_rows_string:
-            self.matrix.append(list(map(int, row.split())))
-        print(self.matrix)
+        matrix_rows_string = (matrix_string.splitlines())
+        self.matrix = [list(map(int, row.split())) for row in matrix_rows_string]
 
     def row(self, index):
         return self.matrix[index-1]
 
     def column(self, index):
-        column = []
-        for row in self.matrix:
-            column.append(row[index-1])
-        return column
+        return [row[index-1] for row in self.matrix]
