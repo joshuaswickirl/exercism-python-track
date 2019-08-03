1ed0f97ca5a0750f011ec7f7d2c736aad166aa3c
Joshua Swick <joshuaswickirl@gmail.com>
2019-08-03 13:37:50 -0600

    Refactor to use more list comp and .row now returns a new list

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,10 +1,10 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         matrix_rows_string = (matrix_string.splitlines())
-        self.matrix = [list(map(int, row.split())) for row in matrix_rows_string]
+        self.matrix = [[int(num) for num in row.split()] for row in matrix_rows_string]
 
     def row(self, index):
-        return self.matrix[index-1]
+        return self.matrix[index-1].copy()
 
     def column(self, index):
         return [row[index-1] for row in self.matrix]
