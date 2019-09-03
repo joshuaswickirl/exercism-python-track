180979551200801b8d0ee93b4695b00aad2000ee
Joshua Swick <joshuaswickirl@gmail.com>
2019-09-02 18:34:28 -0600

    Refactor to use list comp and zip()

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,12 +1,5 @@
 def distance(strand_a, strand_b):
-    if len(strand_a) == len(strand_b):
-        strand_a_list = list(strand_a)
-        strand_b_list = list(strand_b)
-        hamming_distance = 0
-        for index in range(len(strand_a_list)):
-            if strand_a_list[index] != strand_b_list[index]:
-                hamming_distance += 1
-    else:
+    if len(strand_a) != len(strand_b):
         raise ValueError("strand_a and strand_b are of different length")
-    return hamming_distance
-    
+
+    return sum(1 for (letter_a, letter_b) in zip(strand_a, strand_b) if letter_a != letter_b)
